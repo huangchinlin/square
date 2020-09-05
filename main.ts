f284866affafc94a5b1962437cc4da7a055eefa7
huangchinlin <huangchin.lin@gmail.com>
2020-09-05 13:28:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    turtle.pen(TurtlePenMode.Down)
+    for (let index = 0; index <= 4; index++) {
+        turtle.forward(1)
+        turtle.turnRight()
+    }
+})
+turtle.forward(1)
+turtle.turnRight()
+turtle.forward(1)
+turtle.turnRight()
+turtle.forward(1)
+turtle.turnRight()
+turtle.forward(1)
+turtle.turnRight()
+basic.forever(function () {
+	
+})
